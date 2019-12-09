4c1c7537ce71fb36ddb900133ba99b55c57aab48
Benjamin Seah <sbhbenjamin@gmail.com>
2019-12-09 09:48:20 +0000

    add mongo atlas

--- app.js
+++ app.js
@@ -20,9 +20,19 @@ var commentRoutes      = require("./routes/comments"),
 	campgroundRoutes   = require("./routes/campgrounds"),
 	indexRoutes        = require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {
-useUnifiedTopology: true,
-useNewUrlParser: true,
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {
+// useUnifiedTopology: true,
+// useNewUrlParser: true,
+// });
+
+mongoose.connect("mongodb+srv://sbhbenjamin:Spyderseven@cluster0-sypth.mongodb.net/test?retryWrites=true&w=majority",{
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR: ", err.message);
 });
 
 app.use(bodyParser.urlencoded({extended:true}));
