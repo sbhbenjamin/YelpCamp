6e3375b46b55e0bd6abbfdd95f5ccb66c2dd0d80
Ben <sbhbenjamin@gmail.com>
2020-01-07 19:43:49 +0800

    Update Google Maps API Key

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,7 +25,7 @@ router.get("/", function(req, res){
 	});
 });
 
-//CREATE - add new campground to DB
+// CREATE - add new campground to DB
 router.post("/", middleware.isLoggedIn, function(req, res){
   // get data from form and add to campgrounds array
   var name = req.body.name;
