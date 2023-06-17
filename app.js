4d29b024567ae1ac0b57bfc379781ca3d06c86d0
sbhbenjamin <sbhbenjamin@gmail.com>
2023-06-17 16:56:07 +0200

    Prepare for deployment to render

--- app.js
+++ app.js
@@ -1,71 +1,76 @@
 require('dotenv').config();
 
-var express 	  = require("express"),
-	app 	      = express(),
-	bodyParser    = require("body-parser"),
-	mongoose      = require("mongoose"),
-	flash         = require("connect-flash"),
-	passport 	  = require("passport"),
-	LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override"),
-	Campground 	  = require("./models/campground"),
-	Comment   	  = require("./models/comment"),
-	User          = require("./models/user"),
-	seedDB        = require("./seeds");
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  flash = require('connect-flash'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  methodOverride = require('method-override'),
+  Campground = require('./models/campground'),
+  Comment = require('./models/comment'),
+  User = require('./models/user'),
+  seedDB = require('./seeds');
 
 const PORT = process.env.PORT || 3000;
 
-app.locals.moment = require("moment");
+app.locals.moment = require('moment');
 
 // requiring routes
-var commentRoutes      = require("./routes/comments"),
-	campgroundRoutes   = require("./routes/campgrounds"),
-	indexRoutes        = require("./routes/index")
+var commentRoutes = require('./routes/comments'),
+  campgroundRoutes = require('./routes/campgrounds'),
+  indexRoutes = require('./routes/index');
 
 // set default url
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v10"
+var url = process.env.MONGO_URI || 'mongodb://localhost:27017/yelp_camp_v10';
 
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
-}).then(() => {
-	console.log("Connected to DB!");
-}).catch(err => {
-	console.log("ERROR: ", err.message);
-});
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to DB!');
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err.message);
+  });
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine", "ejs")
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 app.use(flash());
-// seedDB(); // seed the database
+seedDB(); // seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Once again Rusty wins cutest dog",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require('express-session')({
+    secret: 'Once again Rusty wins cutest dog',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // avails the user variable
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
-})
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
 
-app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(PORT, function(){
-	console.log(`Server is listening on port ${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}`);
 });
